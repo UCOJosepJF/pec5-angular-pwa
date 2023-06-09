17ade6a5b3b9d3d7856eba1b831cdc3b213945cd
Josep Janer Florit <josepjf@uoc.edu>
2023-06-09 17:44:59 +0200

    pec5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +25,6 @@ import { CdkTableModule } from '@angular/cdk/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatTabsModule } from '@angular/material/tabs';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -51,12 +50,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     CdkTableModule,
     MatProgressSpinnerModule,
     MatTabsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
